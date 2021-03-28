6451f086427e96aee1115ef6418f16f2f848e056
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-03-28 21:09:47 +0700

    ++Finish

--- Second Pactical Midterm Test/Second Pactical Midterm Test/Program.cs
+++ Second Pactical Midterm Test/Second Pactical Midterm Test/Program.cs
@@ -6,7 +6,28 @@ namespace Second_Pactical_Midterm_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a, b, x, y;
+            Console.Write("Please input a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Please input b: ");
+            b = int.Parse(Console.ReadLine());
+
+            x = a;
+            y = b;
+            while (x != y)
+            {
+                if (x < y) 
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(x);
         }
     }
 }
