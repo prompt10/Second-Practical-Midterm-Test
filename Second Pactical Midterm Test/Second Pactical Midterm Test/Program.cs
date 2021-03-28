0a3f534fc4d8efd5fce4fe6c56faf7afe7ec4a9d
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-03-28 20:45:56 +0700

    ++Start the project

---
+++ Second Pactical Midterm Test/Second Pactical Midterm Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Second_Pactical_Midterm_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
